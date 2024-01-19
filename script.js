2b276063efbae56417a85ca9fa148a58705017b2
Broteach <73153880+khmer-embed@users.noreply.github.com>
2024-01-19 13:46:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+const playerInstance = jwplayer("player").setup({
+  controls: true,
+  displaytitle: true,
+  displaydescription: true,
+  abouttext: "JWplayer",
+  aboutlink: "https://khmoviehd.com/",
+
+  skin: {
+    name: "netflix"
+  },
+
+  logo: {
+    file:
+      "",
+    link: ""
+  },
+
+  captions: {
+    color: "#FFF",
+    fontSize: 14,
+    backgroundOpacity: 0,
+    edgeStyle: "raised"
+  },
+
+  playlist: [
+    {
+      title: "",
+      description: "You're Watching",
+      image: "",
+      sources: [
+        {
+          file:
+            "https://raw.githubusercontent.com/khmer-embed/FightsBreakSphereSeason2Episode4/master/FightsBreakSphereSeason2Episode4.m3u8",
+          label: "1080p",
+          default: true
+        }
+      ],
+      captions: [
+        {
+          file:
+            "",
+          label: "Khmer",
+          kind: "captions"
+        },
+        {
+          file:
+            "",
+          label: "English",
+          kind: "captions",
+          default: true
+        }
+      ],
+      tracks: [
+        {
+          file: "",
+          kind: ""
+        }
+      ]
+    }
+  ],
+  advertising: {
+    client: "vast",
+    schedule: [
+      {
+        offset: "pre",
+        tag:
+          ""
+      }
+    ]
+  }
+});
+
+playerInstance.on("ready", function () {
+  const buttonId = "download-video-button";
+  const iconPath =
+    "";
+  const tooltipText = "Download Video";
+
+  // Call the player's `addButton` API method to add the custom button
+  playerInstance.addButton(iconPath, tooltipText, buttonClickAction, buttonId);
+
+  // This function is executed when the button is clicked
+  function buttonClickAction() {
+    const playlistItem = playerInstance.getPlaylistItem();
+    const fileUrl = playlistItem.file;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  // Move the timeslider in-line with other controls
+  const playerContainer = playerInstance.getContainer();
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const spacer = buttonContainer.querySelector(".jw-spacer");
+  const timeSlider = playerContainer.querySelector(".jw-slider-time");
+  buttonContainer.replaceChild(timeSlider, spacer);
+
+  // Detect adblock
+  playerInstance.on("adBlock", () => {
+    const modal = document.querySelector("div.modal");
+    modal.style.display = "flex";
+
+    document
+      .getElementById("close")
+      .addEventListener("click", () => location.reload());
+  });
+
+  // Forward 10 seconds
+  const rewindContainer = playerContainer.querySelector(
+    ".jw-display-icon-rewind"
+  );
+  const forwardContainer = rewindContainer.cloneNode(true);
+  const forwardDisplayButton = forwardContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  forwardDisplayButton.style.transform = "scaleX(-1)";
+  forwardDisplayButton.ariaLabel = "Forward 10 Seconds";
+  const nextContainer = playerContainer.querySelector(".jw-display-icon-next");
+  nextContainer.parentNode.insertBefore(forwardContainer, nextContainer);
+
+  // control bar icon
+  playerContainer.querySelector(".jw-display-icon-next").style.display = "none"; // hide next button
+  const rewindControlBarButton = buttonContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  const forwardControlBarButton = rewindControlBarButton.cloneNode(true);
+  forwardControlBarButton.style.transform = "scaleX(-1)";
+  forwardControlBarButton.ariaLabel = "Forward 10 Seconds";
+  rewindControlBarButton.parentNode.insertBefore(
+    forwardControlBarButton,
+    rewindControlBarButton.nextElementSibling
+  );
+
+  // add onclick handlers
+  [forwardDisplayButton, forwardControlBarButton].forEach((button) => {
+    button.onclick = () => {
+      playerInstance.seek(playerInstance.getPosition() + 10);
+    };
+  });
+});
